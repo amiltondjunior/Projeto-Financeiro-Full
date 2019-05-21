a7176dd3d0bb674e7677b2278f085ce379dbaffd
Amilton Junior <amiltongomes@hotmail.com>
2019-05-21 15:03:57 -0300

    adding navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+//import { listLazyRoutes } from '@angular/compiler/src/aot/lazy_routes';
+//import { format } from 'url';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'categories', loadChildren: './pages/categories/categories.module#CategoriesModule'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
